fca70ae5085099cd8f9197cf71b0173b3e8424aa
Alexander Ostankov <sashaostankov@gmail.com>
2015-11-10 20:22:55 +0300

    Initial check-in of module HammockVisualizer

---
+++ HammockVisualizer/HammockViz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Graphviz4Net.WPF;
+using Graphviz4Net.Graphs;
+using System.ComponentModel;
+using HammockFinder;
+using PNEditorLib;
+
+namespace HammockVisualizer
+{
+    // Class 'GraphInfo' will be used to represent a node in the graph
+
+    /// <summary>
+    ///  Edge in the graph
+    /// </summary>
+    class Edge { }
+
+    class HammockViz
+    {
+        private Graph<GraphInfo> Graph;
+         
+        public HammockViz()
+        {
+
+        }
+    }
+}
